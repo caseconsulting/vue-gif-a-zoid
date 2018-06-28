c036a1a8ac2202e3934fa2f8bb860f0a024049c5
Andrew Cooper <acooper@consultwithcase.com>
2018-06-28 12:08:36 -0400

    initial commit

---
+++ src/views/About.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="about">
+    <h2>This is a Pokemon page</h2>
+    <PokeForm></PokeForm>
+
+  </div>
+</template>
+<script>
+import PokeForm from "@/components/PokeForm.vue";
+
+export default {
+  name: "about",
+  components: {
+    PokeForm
+    //PokeList
+  }
+};
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="home">
+    <img src="../assets/logo-banner.gif"/>
+    <HelloWorld msg="Case Consulting TSheets Portal"/>
+    <Form/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from "@/components/HelloWorld.vue";
+import Form from "@/components/Form.vue";
+
+export default {
+  name: "home",
+  components: {
+    HelloWorld,
+    Form
+  }
+};
+</script>
